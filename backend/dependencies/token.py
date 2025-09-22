1abb769c786d7ac7b0439b70e3957311c310e83e
Joao Gustavo Kowacic Carvalho <joao.kowacic@gmail.com>
2025-09-22 00:40:24 -0300

    added real secret token

--- backend/dependencies/token.py
+++ backend/dependencies/token.py
@@ -1,6 +1,9 @@
 from typing import Annotated
 from fastapi import Header, HTTPException
+import os
+
+SECRET_TOKEN = os.getenv('SECRET_TOKEN')
 
 async def get_token_header(x_token: Annotated[str, Header()]):
-    if x_token != "fake-super-secret-token":
+    if x_token != SECRET_TOKEN:
         raise HTTPException(status_code=400, detail="X-Token header invalid")
